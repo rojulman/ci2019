6feb6df722922fbe9957d55fd08e00abb02c07d4
Sirojul Munir <rojulman@gmail.com>
2019-12-28 12:33:22 +0700

    model departemen add functional crud

--- models/M_departemen.php
+++ models/M_departemen.php
@@ -7,5 +7,26 @@ class M_departemen extends CI_Model{
      	return $this->db->get('departemen');
      }
 
+     function input_data($table,$data){
+     	// INSERT INTO departemen values bla bla ..
+     	$this->db->insert($table,$data);
+     }
+
+     function get_id($table,$idx){
+     	// SELECT * FROM departemen WHERE id=$idx
+     	$this->db->where('id',$idx);
+     	return $this->db->get($table)->row();
+     }
+
+     function update($table,$data,$idx){
+     	$this->db->where('id',$idx);
+     	$this->db->update($table,$data);
+     }
+
+     function delete($table,$idx){
+     	$this->db->where('id',$idx);
+     	$this->db->delete($table);
+     }
+
 
 }

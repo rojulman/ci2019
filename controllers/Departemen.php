54b6dc739094880d531ee5a157f567d1b3621001
Sirojul Munir <rojulman@gmail.com>
2019-12-28 12:32:45 +0700

    update departemen

--- controllers/Departemen.php
+++ controllers/Departemen.php
@@ -19,4 +19,59 @@ $data['deps']=$this->m_departemen->ambil_data()->result();
 
     }
 
+    public function form(){
+
+        $data['judul']='Form Departemen';
+        $this->load->view('departemen/form',$data);
+
+    }
+
+    public function add(){
+        // tangkap data kiriman form
+        $_nama = $this->input->post('nama');
+        $_abbr = $this->input->post('abbr');
+        $_alamat = $this->input->post('alamat');
+        $_telpon = $this->input->post('telpon');
+       
+        // simpan dalam array
+        $data = [
+            'nama'=>$_nama,
+            'abbr'=>$_abbr,
+            'alamat'=>$_alamat,
+            'telpon'=>$_telpon
+        ];
+
+        // input ke table departemen
+$this->m_departemen->input_data('departemen',$data);        // redirect page ke departemen/index
+        redirect('../departemen');
+    }
+
+    public function edit($idx){
+
+    $obj = $this->m_departemen->get_id('departemen',$idx);
+    $data['judul']='Edit Departemen';
+    $data['dep']=$obj;
+    $this->load->view('departemen/edit',$data);
+
+    }
+
+    public function update($idx){
+        $_nama = $this->input->post('nama');
+        $_abbr = $this->input->post('abbr');
+        $_alamat = $this->input->post('alamat');
+        $_telpon = $this->input->post('telpon');
+       
+        // simpan dalam array
+        $data = [
+            'nama'=>$_nama,
+            'abbr'=>$_abbr,
+            'alamat'=>$_alamat,
+            'telpon'=>$_telpon
+        ];
+        $this->m_departemen->update('departemen',$data,$idx);
+        redirect('../departemen');
+
+    }
+
+
 }
